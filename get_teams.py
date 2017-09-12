5fd6ea74f4f2ead182fd2bf446a7e40de2e8f665
Michael Eichenberger <mikeetpt@gmail.com>
2017-09-11 22:05:07 -0500

    Initial Commit

---
+++ get_teams.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+Python webscrapping module.
+
+Webscrap teamrankings.com website to obtain a list of the top 68 basketball
+teams and url references to use for webscrapping other websites for team stats.
+"""
+from itertools import islice
+
+import requests
+from bs4 import BeautifulSoup
+
+# assign url to variable and use requests to get html
+url = 'https://www.teamrankings.com/ncb/'
+teams_page = requests.get(url)
+teams = teams_page.content
+
+# parse html with BeautifulSoup
+soup = BeautifulSoup(teams, 'html.parser')
+
+# scrap all team names and add to a dictionary
+full_teams_list = {}
+lst = soup.find_all('div', {'class': 'table-team-logo-text'})
+for item in lst:
+    team_name = item.find('a').text
+    # scrap url reference for team name (e.g., 'gonzaga-bulldogs')
+    url_ref = item.find('a')['href'][22:]
+    full_teams_list[team_name] = url_ref
+
+
+def take(n, iterable):
+    """Return first n items of the iterable as a list."""
+    return list(islice(iterable, n))
+
+
+# grab top 68 teams and print the team names
+top_68 = take(68, full_teams_list.items())
+for tup in top_68:
+    print('Team: %s\nURL Reference: %s\n' % (tup[0], tup[1]))
