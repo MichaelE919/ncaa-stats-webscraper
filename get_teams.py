af60a6128ca0a0a0149b29fd311e00f8fb7bb9f9
Michael Eichenberger <mikeetpt@gmail.com>
2017-09-13 23:48:13 -0500

    Add code to parse stat pages for all teams and generate stat lists

--- get_teams.py
+++ get_teams.py
@@ -49,3 +49,34 @@ for url in url_ref_list:
                          url + '/stats')
     sos_url_list.append('https://www.teamrankings.com/ncaa-basketball/team/' +
                         url + '/rankings')
+
+first_four = []
+five = []
+for stat in stat_url_list:
+    stats_page = requests.get(stat)
+    stats = stats_page.content
+
+    soup1 = BeautifulSoup(stats, 'html.parser')
+
+    numbers = soup1.find_all('td', {'class': 'nowrap'})
+    s = []
+    for number in numbers:
+        s.append(number.text)
+    st = [
+        s[25][:5], s[125][:5], s[97][:5], s[29][:5], s[27][:5], s[127][:5],
+        s[101][:5], s[31][:5]
+    ]
+    first_four.append(st)
+
+for sos in sos_url_list:
+    rankings_page = requests.get(sos)
+    rankings = rankings_page.content
+
+    soup2 = BeautifulSoup(rankings, 'html.parser')
+
+    numbers2 = soup2.find_all('td', {'class': 'text-right'})
+
+    s1 = []
+    for number2 in numbers2:
+        s1.append(number2.text)
+    five.append(s1[15])
